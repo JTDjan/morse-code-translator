d938e6c476aaf671d2a74b81d5946a6beeac0c35
Your Name <you@example.com>
2021-11-22 17:48:58 +0000

    added button to html & broke down steps in js file

---
+++ main.js
@@ -0,0 +1,22 @@
+//import 
+import {englishWord} from "./transalor.js"
+
+// linking HTML elements from html to js 
+const userInput = document.querySelector("#inputText")
+
+const morseOutput = document.querySelector("#output")
+
+const userBtn = document.querySelector ("#button")
+
+// store the value inputted by the user 
+
+
+// convert the word entered by the user & display it in the output field once the button has been clicked
+
+
+ userBtn.addEventListener ("click", () => {
+
+})
+
+
+
