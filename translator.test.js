69f6000445eea6d9e6757819af803472f25430bc
Your Name <you@example.com>
2021-11-22 14:18:23 +0000

    wrote a function to translate single letters

--- translator.test.js
+++ translator.test.js
@@ -5,6 +5,6 @@ import { expect , it} from "@jest/globals"
 // test that should translate all the letters of the alphabet 
 
 it (" Shoould translate the letter a to . -", () => {
-  const result = translate('a')
+  const result = translate("a")
   expect(result).toBe(".-")
 });
