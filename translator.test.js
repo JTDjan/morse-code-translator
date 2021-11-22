9241b188be463b23cdbc8e5d0d8e644bf431392f
Your Name <you@example.com>
2021-11-22 12:36:01 +0000

    Getting npm test to run

---
+++ translator.test.js
@@ -0,0 +1,10 @@
+// imports of functions & global jest 
+import { translate} from "./transalor.js"
+import { expect , it} from "@jest/globals"
+
+// test that should translate all the letters of the alphabet 
+
+it (" Shoould translate the letter a to . -", () => {
+  const result = translate('a')
+  expect(result).toBe(".-")
+});
